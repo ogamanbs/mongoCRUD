ad4f63baf9c7982f5f2be0a222b5490737ee375d
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-02-14 19:01:24 +0530

    try

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -18,11 +18,10 @@ const getTopics = async () => {
 
 export default async function TopicsList(){
 
-    const { topics } = await getTopics();
-
+    const { topic } = await getTopics();
     return (
         <>
-        { topics.map( (t) => (
+        { topic.map( (t) => (
             <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
                     <h2 className="font-bold text-2xl ">{ t.title }</h2>
