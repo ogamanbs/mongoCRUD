5c649f51d6ed248f77b921d5107e15bc28163d12
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-02-14 18:18:51 +0530

    final commit

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,6 +1,26 @@
 import EditTopicForm from '@/components/EditTopicForm'
-export default function editTopic(){
-    return(
-        <EditTopicForm />
-    )
+
+const getTopicById = async (id) => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+            cache: "no-store",
+        });
+
+        if(!res.ok){
+            throw new Error('Failed to Fetch topic')
+        }
+
+        return res.json();
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default async function editTopic({ params }){
+    const { id } = params;
+    const { topic } = await getTopicById(id);
+    const { title, description } = topic;
+    console.log(topic)
+    return <EditTopicForm id={ id } title={ title } description={ description } />
 }
